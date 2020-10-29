2ad97252dd61a604029e9b4dbf5a7fa29aca6d75
= <atune313@gmail.com>
2020-10-29 14:11:19 +0000

    Initialise spring boot project

---
+++ src/main/java/uk/ac/bristol/cs/co2monitor/Co2monitorApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bristol.cs.co2monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2monitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2monitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/bristol/cs/co2monitor/Co2monitorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bristol.cs.co2monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2monitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
