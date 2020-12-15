1d987240ab9327e68b7c2ff608630988fa35804e
= <atune313@gmail.com>
2020-12-15 13:51:32 +0000

    Switch to new branch

--- co2_monitor_app/android/app/src/main/kotlin/com/example/co2_monitor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.co2_monitor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- co2_monitor_app/test_driver/app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:co2_monitor/dataChart.dart' as app;
-
-void main(){
-  enableFlutterDriverExtension();
-
-  app.main();
-}
