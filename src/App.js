14097bf9f1bcd3f01e4f95f9723d8fc56430fa21
MechyKola <mv.nechyporuk@gmail.com>
2020-12-16 00:33:15 +0000

    some initial graph and things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Grid, Paper } from '@material-ui/core';
+import '../node_modules/react-vis/dist/style.css';
+import {
+  XYPlot,
+  LineSeries
+} from 'react-vis';
+
+const data = [
+  {x: 0, y: 8},
+  {x: 1, y: 5},
+  {x: 2, y: 4},
+  {x: 3, y: 9},
+  {x: 4, y: 1},
+  {x: 5, y: 7},
+  {x: 6, y: 6},
+  {x: 7, y: 3},
+  {x: 8, y: 2},
+  {x: 9, y: 0}
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Grid container 
+          spacing={0}
+          direction="row"
+          justify="center"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
+          <Grid item>
+            <Paper>Another graph</Paper>
+          </Grid>
+          <Grid item>
+            <XYPlot height={300} width={800}>
+              <LineSeries data={data} />
+            </XYPlot>
+          </Grid>
+          <Grid item>
+            <Paper>Maybe some buttons</Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
