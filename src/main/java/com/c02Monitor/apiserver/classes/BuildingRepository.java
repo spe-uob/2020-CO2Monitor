25538a20d58448a1938be36d18a6507f663b6929
GeorgeBannister <56482796+GeorgeBannister@users.noreply.github.com>
2021-02-22 18:59:16 +0000

    Change to JpaRepository

--- src/main/java/com/c02Monitor/apiserver/classes/BuildingRepository.java
+++ src/main/java/com/c02Monitor/apiserver/classes/BuildingRepository.java
@@ -2,9 +2,9 @@ package com.c02Monitor.apiserver.classes;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BuildingRepository extends CrudRepository<Building, Long> {
+public interface BuildingRepository extends JpaRepository<Building, Long> {
 
   public Building findById(String id);
   Long deleteBuildingById(String id);
