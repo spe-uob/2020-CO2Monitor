08eed6bea515163de5582d642028bf80c9463771
GeorgeBannister <56482796+GeorgeBannister@users.noreply.github.com>
2021-02-23 15:43:56 +0000

    fix syntax and dependencies in Db

--- src/main/java/com/c02Monitor/apiserver/controllers/Db.java
+++ src/main/java/com/c02Monitor/apiserver/controllers/Db.java
@@ -1,7 +1,9 @@
-package com.c02Monitor.apiserver.classes;
+package com.c02Monitor.apiserver.controllers;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import com.c02Monitor.apiserver.classes.BuildingRepository;
+import com.c02Monitor.apiserver.classes.Building;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Db {// Controller for interacting with a local mongodb instance
     private BuildingRepository repository;
 
-    public addBuilding(Building x){
-        repository.save(x)
+    public void addBuilding(Building x){
+        repository.save(x);
     }
 
     
