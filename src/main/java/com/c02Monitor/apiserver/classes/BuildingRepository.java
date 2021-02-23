863feeaa42aabcbb9c9e4a680ba7227529f6851a
GeorgeBannister <56482796+GeorgeBannister@users.noreply.github.com>
2021-02-23 15:44:10 +0000

    fix syntax and dependencies in BuildingRepository

--- src/main/java/com/c02Monitor/apiserver/classes/BuildingRepository.java
+++ src/main/java/com/c02Monitor/apiserver/classes/BuildingRepository.java
@@ -1,12 +1,11 @@
 package com.c02Monitor.apiserver.classes;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuildingRepository extends JpaRepository<Building, Long> {
 
-  public Building findById(String id);
-  Long deleteBuildingById(String id);
+  public Building findById(long id);
+  Long deleteBuildingById(long id);
 
 }
