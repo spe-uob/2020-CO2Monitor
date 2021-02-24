ae3ffa6f2a23f87306991a6dfb122df9cd8122f6
GeorgeBannister <56482796+GeorgeBannister@users.noreply.github.com>
2021-02-24 15:24:24 +0000

    Add Put mapping

--- src/main/java/com/c02Monitor/apiserver/controllers/REST.java
+++ src/main/java/com/c02Monitor/apiserver/controllers/REST.java
@@ -94,7 +94,7 @@ class REST {
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////// ADD ONE //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  @PutMapping("/buildings/{id}")
+  @PutMapping("/buildings/{id}")// Add or replace a building
   Building replaceBuilding(@RequestBody Building newBuilding, @PathVariable Long id) {
     
     return repository.findById(id)
@@ -109,4 +109,52 @@ class REST {
         return repository.save(newBuilding);
       });
   }
+
+
+@PutMapping("/buildings/{id}/rooms/{id2}")// Add or replace a room in a building
+Building replaceRoom(@RequestBody Room newRoom, @PathVariable Long id,@PathVariable Long id2) {
+  
+  return repository.findById(id)
+    .map(building -> {
+      building.delRoom(id2);
+      building.AddRoom(newRoom);
+      return repository.save(building);
+    })
+    .orElseGet(() -> {
+      
+      return null;
+    });
+}
+
+@PutMapping("/buildings/{id}/rooms/{id2}/sensors/{id3}")// Add or replace a sensor in a room in a building
+Building replaceSensor(@RequestBody Sensor newSensor, @PathVariable Long id,@PathVariable Long id2,@PathVariable Long id3) {
+  
+  return repository.findById(id)
+    .map(building -> {
+      building.getRoom(id2).delSensor(id3);
+      building.getRoom(id2).addSensor(newSensor);
+      return repository.save(building);
+    })
+    .orElseGet(() -> {
+      
+      return null;
+    });
+}
+
+@PutMapping("/buildings/{id}/rooms/{id2}/sensors/{id3}/data/{id4}")// Add or replace a datum in a sensor in a room in a building
+Building replaceDatum(@RequestBody Data newDatum, @PathVariable Long id,@PathVariable Long id2,@PathVariable Long id3,@PathVariable Long id4) {
+  
+  return repository.findById(id)
+    .map(building -> {
+      building.getRoom(id2).getSensor(id3).delDatum(id4);
+      building.getRoom(id2).getSensor(id3).addData(newDatum);
+      return repository.save(building);
+    })
+    .orElseGet(() -> {
+      
+      return null;
+    });
+}
+
+
 }
