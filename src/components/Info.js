85165d4838d32318cc9a46b0fc42fafece25aba1
MechyKola <mv.nechyporuk@gmail.com>
2021-02-25 13:57:18 +0000

    add sensor popup added

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {
   Button,
   Dialog,
+  DialogTitle,
   DialogContent,
+  DialogActions,
   Grid,
   Paper,
   Slide,
+  TextField,
 } from '@material-ui/core';
 import {Add, Close} from '@material-ui/icons';
 import './Info.css';
@@ -21,14 +24,21 @@ const Transition = React.forwardRef(function Transition(props, ref) {
  */
 export default function Info(props) {
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
+
+  const [openAddSensor, setOpenAddSensor] = React.useState(false);
+  const handleClickOpenAddSensor = () => {
+    setOpenAddSensor(true);
+  };
+  const handleCloseAddSensor = () => {
+    setOpenAddSensor(false);
+  };
+
 
   if (props.sensors.length > 0) {
     console.log(props.sensors[0].sensorId);
@@ -80,9 +90,45 @@ export default function Info(props) {
                 className="Right-header"
                 disableElevation
                 startIcon={<Add />}
+                onClick={handleClickOpenAddSensor}
               >
                 Add sensor
               </Button>
+              <Dialog
+                open={openAddSensor}
+                onClose={handleCloseAddSensor}
+              >
+                <DialogTitle>
+            Add sensor
+                </DialogTitle>
+                <DialogContent>
+                  <div>
+                    <TextField
+                      multiline
+                      label="Sensor ID"
+                      variant="outlined"
+                    />
+                  </div>
+                  <div className="PaddedCard">
+
+                  </div>
+                  <div>
+                    <TextField
+                      multiline
+                      label="Description"
+                      variant="outlined"
+                    />
+                  </div>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleCloseAddSensor}>
+              Cancel
+                  </Button>
+                  <Button color="primary" variant="contained">
+              Add
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </Grid>
           </Grid>
         </Paper>
