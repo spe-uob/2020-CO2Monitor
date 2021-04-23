7652b9704e08697fb8db9c55fd91c7380ea3ab14
OliverPennery <mb19626@bristol.ac.uk>
2021-04-23 15:54:17 +0100

    Create BuildingDTO.java

---
+++ src/main/java/com/c02Monitor/apiserver/dto/BuildingDTO.java
@@ -0,0 +1,31 @@
+package com.c02Monitor.apiserver.dto;
+
+import com.c02Monitor.apiserver.entity.Room;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = BuildingDTO.BuildingDTOBuilder.class)
+//@JsonIgnoreProperties({"hibernate_lazy_initializer", "handler"})
+public class BuildingDTO {
+    private Long id;
+    @JsonProperty("name")
+    private String name;
+    private Set<Room> rooms;
+
+    @Singular private Map<String, String> links;
+
+}
