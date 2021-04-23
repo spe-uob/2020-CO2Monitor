262eabc403d8217a83197041ce2b0f4a7c6850a2
OliverPennery <mb19626@bristol.ac.uk>
2021-04-23 15:52:36 +0100

    added pageable search

---
+++ src/main/java/com/c02Monitor/apiserver/repository/ReadingRepository.java
@@ -0,0 +1,18 @@
+package com.c02Monitor.apiserver.repository;
+
+import com.c02Monitor.apiserver.entity.Reading;
+import com.c02Monitor.apiserver.entity.Room;
+import com.c02Monitor.apiserver.entity.Sensor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ReadingRepository extends JpaRepository<Reading, Long> {
+    List<Reading> findBySensorId(long id);
+    Page<Reading> findBySensorId(long id, Pageable pageable);
+}
