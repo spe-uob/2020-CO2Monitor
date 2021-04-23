9928f6f1481e9daf07179869f8c6630d899db1c7
OliverPennery <mb19626@bristol.ac.uk>
2021-04-23 15:53:24 +0100

    Create BuildingService.java

---
+++ src/main/java/com/c02Monitor/apiserver/service/BuildingService.java
@@ -0,0 +1,45 @@
+package com.c02Monitor.apiserver.service;
+
+import com.c02Monitor.apiserver.dto.BuildingDTO;
+import com.c02Monitor.apiserver.entity.Building;
+import com.c02Monitor.apiserver.repository.BuildingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class BuildingService {
+
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    //GET
+    public Optional<Building> getBuildingById(long id){
+        return buildingRepository.findById(id);
+    }
+
+    public List<Building> getAllBuildings(){
+        return buildingRepository.findAll();
+    }
+
+    //Create
+    public Building createBuilding(Building building){
+        return buildingRepository.save(building);
+    }
+
+    public void createManyBuildings(List<Building> buildings){
+        buildingRepository.saveAll(buildings);
+    }
+
+    //UPDATE
+    public Building updateBuilding(Building building) {return buildingRepository.save(building);}
+
+    //DELETE
+    public void deleteById(Long id) {buildingRepository.deleteById(id);}
+}
