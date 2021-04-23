c7b441d8523c99e0e6f944455e6752544ad51e03
OliverPennery <mb19626@bristol.ac.uk>
2021-04-23 15:53:29 +0100

    Create RoomService.java

---
+++ src/main/java/com/c02Monitor/apiserver/service/RoomService.java
@@ -0,0 +1,48 @@
+package com.c02Monitor.apiserver.service;
+
+import com.c02Monitor.apiserver.dto.RoomDTO;
+import com.c02Monitor.apiserver.entity.Building;
+import com.c02Monitor.apiserver.entity.Room;
+import com.c02Monitor.apiserver.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class RoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    //GET
+    public Optional<Room> getRoomById(long id){
+        return roomRepository.findById(id);
+    }
+
+    public List<Room> getAllBuildingRoomsById(long id){
+        return roomRepository.findByBuildingId(id);
+    }
+
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    //Create
+    public Room createRoom(Room room){
+        return roomRepository.save(room);
+    }
+
+    public void createManyRooms(List<Room> rooms){
+        roomRepository.saveAll(rooms);
+    }
+
+    //UPDATE
+    public Room updateRoom(Room room) {return roomRepository.save(room);}
+
+    //DELETE
+    public void deleteById(Long id) {roomRepository.deleteById(id);}
+}
