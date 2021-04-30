741743cbc43b451fc5d51064f4d3338f46399331
MechyKola <io@mykola.cc>
2021-04-30 13:07:28 +0100

    login ui

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import {
   IconButton,
   InputBase,
   TextField,
-  Snackbar
+  Snackbar,
+  CircularProgress
 } from '@material-ui/core'
 import { Add, Search } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
@@ -92,6 +93,31 @@ for (let i = 0; i < roomNum; i++) {
 function App () {
   const classes = useStyles()
 
+  const [token, setToken] = useState('test')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginError, setLoginError] = useState(<React.Fragment />)
+  const [loginButton, setLoginButton] = useState('Log in')
+
+  const requestToken = () => {
+    setLoginButton(<CircularProgress />)
+    axios.put('https://100.25.147.253:8080/api/v1/login',
+      {
+        username: username,
+        password: password
+      }).then((response) => {
+      setToken(response.data.token)
+    }).catch((error) => {
+      console.log(error)
+      setLoginError(
+          <Alert severity="error">
+            Please log in.
+          </Alert>
+      )
+      setLoginButton('Log in')
+    })
+  }
+
   const [roomCards, setRoomCards] = useState('Loading...')
   const [rooms, setRooms] = useState([])
 
@@ -148,7 +174,6 @@ function App () {
   const changeAddRoomName = (newTerm) => {
     setAddRoomName(newTerm)
   }
-
   const [addRoomBuildingName, setAddRoomBuildingName] = useState('')
   const changeAddRoomBuildingName = (newTerm) => {
     setAddRoomBuildingName(newTerm)
@@ -234,100 +259,139 @@ function App () {
     }
   }
 
-  return (
-    <div className="App">
-      {/* Header bar */}
-      <Paper className="App-header">
+  if (token) {
+    return (
+      <div className="App">
+        {/* Header bar */}
+        <Paper className="App-header">
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item sm={12} md={6}>
+              <a className="Left-header">
+                CO2 Monitor Control Panel
+            </a>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <div className="Right-header">
+                <InputBase
+                  placeholder="Search For Room"
+                  onChange={(event) => changeSearchTerm(event.target.value)}
+                  onKeyDown={(event) => keyPressSearch(event)}
+                />
+                <IconButton onClick={filterBySearchTerm}>
+                  <Search />
+                </IconButton>
+              </div>
+            </Grid>
+          </Grid>
+        </Paper>
+
+        {/* The cards (rooms) */}
         <Grid
           container
           spacing={0}
           direction="row"
-          justify="space-between"
-          alignItems="center"
+          justify="flex-start"
+          alignItems="flex-start"
         >
-          <Grid item sm={12} md={6}>
-            <a className="Left-header">
-              CO2 Monitor Control Panel
-            </a>
-          </Grid>
-          <Grid item sm={12} md={6}>
-            <div className="Right-header">
-              <InputBase
-                placeholder="Search For Room"
-                onChange={(event) => changeSearchTerm(event.target.value)}
-                onKeyDown={(event) => keyPressSearch(event)}
-              />
-              <IconButton onClick={filterBySearchTerm}>
-                <Search />
-              </IconButton>
-            </div>
-          </Grid>
+          {roomCards}
         </Grid>
-      </Paper>
 
-      {/* The cards (rooms) */}
-      <Grid
-        container
-        spacing={0}
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-      >
-        {roomCards}
-      </Grid>
+        {/* Spain but the s is silent */}
+        <Fab
+          className={classes.addRoomTheme}
+          onClick={handleClickOpenAddRoom}
+          data-testid="add-room-button"
+        >
+          <Add />
+        </Fab>
+        <Dialog
+          open={openAddRoom}
+          onClose={handleCloseAddRoom}
+        >
+          <DialogTitle>
+            Add Room
+        </DialogTitle>
+          <DialogContent>
+            <TextField
+              label="name"
+              variant="outlined"
+              onChange={(event) => changeAddRoomName(event.target.value)}
+            />
+            <div className="PaddedCard" />
+            <TextField
+              label="building"
+              variant="outlined"
+              onChange={(event) => changeAddRoomBuildingName(event.target.value)}
+              onKeyDown={(event) => keyPressAddRoom(event)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseAddRoom} data-testid="cancel-add-room">
+              Cancel
+          </Button>
+            <Button onClick={addRoom} color="primary" variant="contained">
+              Add
+          </Button>
+          </DialogActions>
 
-      {/* Spain but the s is silent */}
-      <Fab
-        className={classes.addRoomTheme}
-        onClick={handleClickOpenAddRoom}
-        data-testid="add-room-button"
-      >
-        <Add />
-      </Fab>
+          <Snackbar open={addRoomSuccess} autoHideDuration={6000} onClose={addRoomSuccessClose}>
+            <Alert onClose={addRoomSuccessClose} severity="success">
+              Room Added! Refresh the page to see changes.
+          </Alert>
+          </Snackbar>
+
+          <Snackbar open={addRoomError} autoHideDuration={6000} onClose={addRoomErrorClose}>
+            <Alert onClose={addRoomErrorClose} severity="error">
+              Room could not be added.
+          </Alert>
+          </Snackbar>
+        </Dialog>
+      </div>
+    )
+  } else {
+    return (
+    <>
       <Dialog
-        open={openAddRoom}
-        onClose={handleCloseAddRoom}
+        open={true}
       >
         <DialogTitle>
-          Add Room
-        </DialogTitle>
+          Log in
+      </DialogTitle>
         <DialogContent>
           <TextField
-            label="name"
+            label="username"
             variant="outlined"
-            onChange={(event) => changeAddRoomName(event.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <div className="PaddedCard" />
           <TextField
-            label="building"
+            label="password"
             variant="outlined"
-            onChange={(event) => changeAddRoomBuildingName(event.target.value)}
-            onKeyDown={(event) => keyPressAddRoom(event)}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                requestToken()
+              }
+            }}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleCloseAddRoom} data-testid="cancel-add-room">
-            Cancel
-          </Button>
-          <Button onClick={addRoom} color="primary" variant="contained">
-            Add
+          <Button onClick={requestToken} color="primary" variant="contained">
+            {loginButton}
           </Button>
         </DialogActions>
-
-        <Snackbar open={addRoomSuccess} autoHideDuration={6000} onClose={addRoomSuccessClose}>
-          <Alert onClose={addRoomSuccessClose} severity="success">
-            Room Added! Refresh the page to see changes.
-          </Alert>
-        </Snackbar>
-
-        <Snackbar open={addRoomError} autoHideDuration={6000} onClose={addRoomErrorClose}>
-          <Alert onClose={addRoomErrorClose} severity="error">
-            Room could not be added.
-          </Alert>
-        </Snackbar>
+        {loginError}
       </Dialog>
-    </div>
-  )
+    </>
+    )
+  }
 }
 
 export default App
