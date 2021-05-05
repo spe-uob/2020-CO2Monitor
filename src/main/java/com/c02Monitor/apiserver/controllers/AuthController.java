638745e62933e1bd2a4412c4a3dfb04f2e0bd8b8
OliverPennery <mb19626@bristol.ac.uk>
2021-05-05 22:30:18 +0100

    Create AuthController.java

---
+++ src/main/java/com/c02Monitor/apiserver/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package com.c02Monitor.apiserver.controllers;
+
+import com.c02Monitor.apiserver.security.AuthRequest;
+import com.c02Monitor.apiserver.security.AuthResponse;
+import com.c02Monitor.apiserver.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @GetMapping("")
+    public ResponseEntity<?> keyChecker(){
+        return new ResponseEntity<>("Valid Key", HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthRequest authenticationRequest) throws Exception {
+        try {
+            var authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+            final String token = jwtTokenUtil.createToken(authentication);
+            return ResponseEntity.ok(new AuthResponse(token));
+        }
+        catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid username/password supplied", HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
