1734d7e9306d5000ee5114fa296f0b79ca255b7b
OliverPennery <mb19626@bristol.ac.uk>
2021-05-05 22:35:03 +0100

    Create JwtRequestFilter.java

---
+++ src/main/java/com/c02Monitor/apiserver/security/JwtRequestFilter.java
@@ -0,0 +1,44 @@
+package com.c02Monitor.apiserver.security;
+
+import com.c02Monitor.apiserver.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        String token = null;
+        String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+        }
+        
+        if (token != null && jwtUtil.validateToken(token)) {
+            Authentication auth = jwtUtil.getAuthentication(token);
+
+            if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(req, res);
+    }
+}
