44af9ff17db01f8e9ddc5aa37400e7a51c62697b
OliverPennery <mb19626@bristol.ac.uk>
2021-05-05 22:26:45 +0100

    Create UserRepository.java

---
+++ src/main/java/com/c02Monitor/apiserver/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.c02Monitor.apiserver.repository;
+
+
+import com.c02Monitor.apiserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
