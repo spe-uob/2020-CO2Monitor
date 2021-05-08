a8feeaeeac9b54a3e2560685ff7699e2c5153e68
Unknown <unknown@example.com>
2021-05-08 17:22:18 +0100

    Add info to deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
+
 # Make sure that mysql server processes are running.
 /etc/init.d/mysql start
 service mysql start
 
+# Get public ip 
+ip = $(curl ifconfig.me)
+# Get mysql port
+port = $(lsof -i -P -n | grep LISTEN | grep mysql | grep \*)
+
+echo "My public ip = " $ip /n
+
+echo "Info about mysql port:/n" $port
 # Run the Spring-Boot program (should change this for final build)
 ./mvnw spring-boot:run
