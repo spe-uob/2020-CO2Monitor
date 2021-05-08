2a829ba1c1d5dde9995daba06625584dbe3c553b
Unknown <unknown@example.com>
2021-05-08 17:31:51 +0100

    Tidy up

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,11 @@
 # Make sure that mysql server processes are running.
 /etc/init.d/mysql start
 service mysql start
-# Get public ip 
+# Show the user their public ip
 echo "My public ip = "
 curl ifconfig.me
-echo $ip
-# Get mysql port
-echo "Info about mysql port:\n"
+# Show user the mysql port
+echo "Info about mysql port:"
 lsof -i -P -n | grep LISTEN | grep mysql | grep \*
 # Run the Spring-Boot program (should change this for final build)
 ./mvnw spring-boot:run
