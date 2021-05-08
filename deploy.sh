bd1885ddfc3805bf1fe58ef41fbe606d7ddbffb1
Unknown <unknown@example.com>
2021-05-08 17:30:17 +0100

    stop using variables

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,12 @@
 # Make sure that mysql server processes are running.
 /etc/init.d/mysql start
 service mysql start
-
 # Get public ip 
-ip = $(curl ifconfig.me)
-# Get mysql port
-port = $(lsof -i -P -n | grep LISTEN | grep mysql | grep \*)
-
 echo "My public ip = "
+curl ifconfig.me
 echo $ip
-
-echo "Info about mysql port:/n"
-echo $port
+# Get mysql port
+echo "Info about mysql port:\n"
+lsof -i -P -n | grep LISTEN | grep mysql | grep \*
 # Run the Spring-Boot program (should change this for final build)
 ./mvnw spring-boot:run
