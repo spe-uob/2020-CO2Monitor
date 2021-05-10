167b56db4bd4ff8c14015279f8ffecb3a399e126
GeorgeBannister <56482796+GeorgeBannister@users.noreply.github.com>
2021-05-10 23:59:18 +0100

    RoomControllerTests

--- src/test/java/com/c02Monitor/apiserver/controllers/RoomControllerTest.java
+++ src/test/java/com/c02Monitor/apiserver/controllers/RoomControllerTest.java
@@ -1,80 +1,222 @@
 package com.c02Monitor.apiserver.controllers;
-/*
-import com.c02Monitor.apiserver.classes.Room;
-import com.c02Monitor.apiserver.classes.Sensor;
-import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoField;
-import java.util.HashSet;
-import java.util.Set;
+import com.c02Monitor.apiserver.dto.BuildingDTO;
+import com.c02Monitor.apiserver.dto.RoomDTO;
+import com.c02Monitor.apiserver.entity.Building;
+import com.c02Monitor.apiserver.controllers.ParentController;
+import com.c02Monitor.apiserver.entity.Room;
+import com.c02Monitor.apiserver.entity.Sensor;
+import com.c02Monitor.apiserver.entity.Reading;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
 class RoomControllerTest {
 
-    //not null test
-    @Test
-    public void getAllRoomsTest() {
-        RoomController controller = new RoomController();
-        controller.getAllRooms().forEach(x -> assertTrue(x.getId() >= 0));
-        controller.getAllRooms().forEach(x -> assertNotNull(x.getName()));
-        controller.getAllRooms().forEach(x -> assertTrue(x.getSensors().isEmpty()));
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private BuildingController controller;
+    @Autowired
+    private RoomController controller2;
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", "fooClientIdPassword");
+        params.add("username", username);
+        params.add("password", password);
+
+        ResultActions result
+                = mockMvc.perform(post("/api/v1/auth")
+                .params(params)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\":\"foo\",\"password\":\"bar\"}")
+                .accept("application/json;charset=UTF-8"));
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("token").toString();
     }
 
-    //duplicate id test
+
     @Test
-    public void RoomDuplicateIdTest() {
-        RoomController controller = new RoomController();
-        Set<Long> ids = new HashSet<Long>();
-        assertTrue(controller.getAllRooms().stream()
-                .map(Room::getId)
-                .filter(x -> !ids.add(x))
-                .collect(Collectors.toSet())
-                .isEmpty());
+    public void createRoomNoAuthTest() throws Exception {
+        Room test = new Room();
+        RoomDTO testDTO = controller2.convertToDTO(test);
+        this.mockMvc.perform(post("/api/v1/rooms")
+                .header("Authorization", "Bearer " + "invalidtoken..")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testDTO)))
+                .andExpect(status().isForbidden());
 
     }
 
-    //not null test
     @Test
-    public void getRoomDetailTest() {
-        RoomController controller = new RoomController();
-        controller.getAllRooms().stream()
-                .map(x -> controller.getRoomDetail(x.getId()))
-                .forEach(x -> {
-                    assertTrue(x.getId() >= 0);
-                    assertNotNull(x.getName());
-                    assertFalse(x.getSensors().isEmpty());
-                });
+    public void updateRoomNoAuthTest() throws Exception {
+        Room test = new Room();
+        RoomDTO testDTO = controller2.convertToDTO(test);
+        this.mockMvc.perform(put("/api/v1/rooms/" + test.getId())
+                .header("Authorization", "Bearer " + "invalidtoken..")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testDTO)))
+                .andExpect(status().isForbidden());
     }
 
-    //date valid test
     @Test
-    public void  getRoomDetailSensorTest() {
-        RoomController controller = new RoomController();
-        controller.getAllRooms().forEach(y -> {
-            controller.getRoomDetail(y.getId()).getSensors().forEach(x -> {
-                assertTrue(x.getId() >= 0);
-                assertNotNull(x.getDescription());
-                assertTrue(x.getData().isEmpty());
-            });
-        });
+    public void deleteRoomNoAuthTest() throws Exception {
+        this.mockMvc.perform(delete("/api/v1/rooms/")
+                .header("Authorization", "Bearer " + "invalidtoken..")
+                .content("") )
+                .andExpect(status().isForbidden());
     }
 
-    //duplicate id test
     @Test
-    public void getRoomDetailSensorDuplicateIdTest() {
-        RoomController controller = new RoomController();
-        controller.getAllRooms().forEach(y -> {
-            Set<Long> ids = new HashSet<Long>();
-            assertTrue(controller.getRoomDetail(y.getId()).getSensors().stream()
-                    .map(Sensor::getId)
-                    .filter(x -> !ids.add(x))
-                    .collect(Collectors.toSet())
-                    .isEmpty());
-        });
-    }
-}
+    public void createRoomAuthTest() throws Exception {
+        String accessToken = obtainAccessToken("foo", "bar");
+        Building mvb = new Building("test");
+        mvb.setId(123123);
+        BuildingDTO mvbDTO = controller.convertToDTO(mvb);
 
- */
+        MvcResult result = mockMvc.perform(post("/api/v1/buildings/")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(mvbDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        String id = jsonParser.parseMap(content).get("id").toString();
+
+        mvb.setId(Long.parseLong(id));
+        System.out.println(mvb.getId()+ "\n\n\n\n\n\n\n");
+        Room test = new Room("test", mvb);
+        RoomDTO testDTO = controller2.convertToDTO(test);
+        this.mockMvc.perform(post("/api/v1/buildings/" + id + "/rooms")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateRoomAuthTest() throws Exception {
+        String accessToken = obtainAccessToken("foo", "bar");
+        Building mvb = new Building("test");
+        mvb.setId(123123);
+        BuildingDTO mvbDTO = controller.convertToDTO(mvb);
+
+        MvcResult result = mockMvc.perform(post("/api/v1/buildings/")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(mvbDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        String id = jsonParser.parseMap(content).get("id").toString();
+
+        mvb.setId(Long.parseLong(id));
+        System.out.println(mvb.getId()+ "\n\n\n\n\n\n\n");
+        Room test = new Room("test", mvb);
+        RoomDTO testDTO = controller2.convertToDTO(test);
+        MvcResult result2 = mockMvc.perform(post("/api/v1/buildings/" + id + "/rooms/")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content2 = result.getResponse().getContentAsString();
+        String id2 = jsonParser.parseMap(content).get("id").toString();
+
+        mockMvc.perform(put("/api/v1/buildings/" + id + "/rooms/" + id2)
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(mvbDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void deleteRoomAuthTest() throws Exception { //TODO
+        String accessToken = obtainAccessToken("foo", "bar");
+        Building mvb = new Building("test");
+        mvb.setId(123123);
+        BuildingDTO mvbDTO = controller.convertToDTO(mvb);
+
+        MvcResult result = mockMvc.perform(post("/api/v1/buildings/")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(mvbDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        String id = jsonParser.parseMap(content).get("id").toString();
+
+        mvb.setId(Long.parseLong(id));
+        System.out.println(mvb.getId()+ "\n\n\n\n\n\n\n");
+        Room test = new Room("test", mvb);
+        RoomDTO testDTO = controller2.convertToDTO(test);
+        MvcResult result2 = mockMvc.perform(post("/api/v1/buildings/" + id + "/rooms/")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content2 = result.getResponse().getContentAsString();
+        String id2 = jsonParser.parseMap(content).get("id").toString();
+
+        mockMvc.perform(delete("/api/v1/buildings/" + id + "/rooms/" + id2)
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(mvbDTO))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    }
+
+
