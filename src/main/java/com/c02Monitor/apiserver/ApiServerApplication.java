37beeb02fcca0c4ca0042be1e823a47ba26f5b86
OliverPennery <mb19626@bristol.ac.uk>
2021-05-11 09:06:44 +0100

    remove init data

--- src/main/java/com/c02Monitor/apiserver/ApiServerApplication.java
+++ src/main/java/com/c02Monitor/apiserver/ApiServerApplication.java
@@ -29,29 +29,29 @@ public class ApiServerApplication {
 	@Bean
 	CommandLineRunner runner(BuildingService buildingService, RoomService roomService, SensorService sensorService, UserService userService, ApiServerApplication bar){
 		return args -> {
-			User user = new User("foo", "bar");
-			userService.create(user);
-
-			Building building1 =  new Building("MVB");
-			Building building2 =  new Building("Queens");
-
-			buildingService.createBuilding(building1);
-			buildingService.createBuilding(building2);
-
-			Room room1 = new Room("1.1", building1);
-			Room room2 = new Room("2.11", building1);
-			roomService.createRoom(room1);
-			roomService.createRoom(room2);
-
-			Room room3 = new Room("Room A", building2);
-			Room room4 = new Room("Room B", building2);
-			roomService.createRoom(room3);
-			roomService.createRoom(room4);
-
-			Sensor sensor1 = new Sensor(12600, "test", room1);
-			Sensor sensor2 = new Sensor(9858, "test", room1);
-			sensorService.createSensor(sensor1);
-			sensorService.createSensor(sensor2);
+//			User user = new User("foo", "bar");
+//			userService.create(user);
+//
+//			Building building1 =  new Building("MVB");
+//			Building building2 =  new Building("Queens");
+//
+//			buildingService.createBuilding(building1);
+//			buildingService.createBuilding(building2);
+//
+//			Room room1 = new Room("1.1", building1);
+//			Room room2 = new Room("2.11", building1);
+//			roomService.createRoom(room1);
+//			roomService.createRoom(room2);
+//
+//			Room room3 = new Room("Room A", building2);
+//			Room room4 = new Room("Room B", building2);
+//			roomService.createRoom(room3);
+//			roomService.createRoom(room4);
+//
+//			Sensor sensor1 = new Sensor(12600, "test", room1);
+//			Sensor sensor2 = new Sensor(9858, "test", room1);
+//			sensorService.createSensor(sensor1);
+//			sensorService.createSensor(sensor2);
 
 			DEVICE_IDS.addAll(sensorService.getAllSensors()
                         .stream()
